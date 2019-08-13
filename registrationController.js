11c25c6ec3a12264592a7a3de206325e6f9a3142
NaveenS1996 <40712538+NaveenS1996@users.noreply.github.com>
2019-08-13 18:54:28 +0530

    Update registrationController.js

--- registrationController.js
+++ registrationController.js
@@ -1,9 +1,37 @@
 const express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+const Registration = mongoose.model('Registration');
 
 //Page Navigation -- Routing
 router.get('/',(req,res) => {
-    res.json('Sample Page');
+    res.render("registration/addUser",{
+        viewTitle : "Insert User"
+    });
+});
+
+router.post('/',(req,res) => {
+    insertData(req,res);
+});
+
+//Function to Insert the New User in DB.
+function insertData(req,res){
+    var registration = new Registration();
+    registration.fullName = req.body.fullName;
+    registration.mail = req.body.mail;
+    registration.mobile = req.body.mobile;
+    registration.department = req.body.department;
+    registration.save((err,doc) => {
+        if(!err)
+        res.redirect('registration/list');
+        else
+        console.log('Error while adding New User : '+err);
+    });
+}
+
+//Routing to Next Page after user registration.
+router.get('/list',(req,res) => {
+    res.json('Next Page');
 });
 
 module.exports = router;
